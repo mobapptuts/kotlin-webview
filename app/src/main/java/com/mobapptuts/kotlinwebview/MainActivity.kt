cf728069b9f570772264b57b2b2c30ab40a3106b
Nigel Henshaw <nigel@mobapptuts.com>
2017-08-11 10:49:39 +0900

    added page loading progress bar and navigation buttons

--- app/src/main/java/com/mobapptuts/kotlinwebview/MainActivity.kt
+++ app/src/main/java/com/mobapptuts/kotlinwebview/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mobapptuts.kotlinwebview
 
+import android.graphics.Bitmap
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.browser_toolbar.*
@@ -20,6 +24,12 @@ class MainActivity : AppCompatActivity() {
                 true
             } else false
         }
+        backButton.setOnClickListener {
+            if(webview.canGoBack()) webview.goBack()
+        }
+        forwardButton.setOnClickListener {
+            if(webview.canGoForward()) webview.goForward()
+        }
     }
 
     @Throws(UnsupportedOperationException::class)
@@ -35,13 +45,39 @@ class MainActivity : AppCompatActivity() {
 
         // Enable javascript
         webview.settings.javaScriptEnabled = true
-        // Keep browsing links inside webview
-        webview.webViewClient = WebViewClient()
+        pageLoadStatus()
+        updateProgress()
         try {
             val uri = buildUri(uriText.text.toString())
             webview.loadUrl(uri.toString())
         } catch(e: UnsupportedOperationException) {
             e.printStackTrace()
+        }
+    }
+
+    fun updateProgress() {
+        webview.webChromeClient = object: WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+
+                pageLoadProgressBar.progress = newProgress
+            }
+        }
+    }
+    fun pageLoadStatus() {
+        webview.webViewClient = object: WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+                pageLoadProgressBar.visibility = View.GONE
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+                pageLoadProgressBar.visibility = View.VISIBLE
+                pageLoadProgressBar.progress = 0
+            }
         }
     }
 }
